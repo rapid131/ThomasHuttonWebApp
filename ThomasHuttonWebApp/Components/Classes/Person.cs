223c6cfba8f6d2f4762763789ce778ada28011a1
rapid131 <thutton2@missouriwestern.edu>
2025-06-16 15:35:11 -0500

    Add ID to Person

--- ThomasHuttonWebApp/Components/Classes/Person.cs
+++ ThomasHuttonWebApp/Components/Classes/Person.cs
@@ -2,6 +2,7 @@
 {
     public class Person
     {
+        public int Id { get; set; }
         public string Company { get; set; }
         public string Name { get; set; }
         public DateTime Datesigned { get; set; }

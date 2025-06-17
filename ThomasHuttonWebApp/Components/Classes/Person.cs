f2151795ed7c91a685806af6093d4019da46be76
rapid131 <thutton2@missouriwestern.edu>
2025-06-17 13:59:34 -0500

    Form Warnings

--- ThomasHuttonWebApp/Components/Classes/Person.cs
+++ ThomasHuttonWebApp/Components/Classes/Person.cs
@@ -1,9 +1,14 @@
-﻿namespace ThomasHuttonWebApp.Components.Classes
+﻿using System.ComponentModel.DataAnnotations;
+namespace ThomasHuttonWebApp.Components.Classes
 {
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Company is required.")]
+        [StringLength(25, ErrorMessage = "Company name must be less than 25 characters.")]
         public string Company { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(15, ErrorMessage = "First name must be less than 15 characters.")]
         public string Name { get; set; }
         public DateTime Datesigned { get; set; }
 

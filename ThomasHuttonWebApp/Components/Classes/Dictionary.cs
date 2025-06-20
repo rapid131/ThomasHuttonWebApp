8475ca4fe9c46893153ed43effcf5ea849b907a4
rapid131 <thutton2@missouriwestern.edu>
2025-06-20 17:38:01 -0500

    Dictionary API (unfinished)

---
+++ ThomasHuttonWebApp/Components/Classes/Dictionary.cs
@@ -0,0 +1,23 @@
+﻿namespace ThomasHuttonWebApp.Components.Classes
+{
+    public class Dictionary
+    {
+        public string word { get; set; }
+        public List<Phonetic> phonetics { get; set; }
+        public List<Meaning> meanings { get; set; }
+        public class Phonetic
+        {
+            public string audio { get; set; }
+            public string sourceUrl { get; set; }
+        }
+        public class Meaning
+        {
+            public List<Definition> definitions { get; set; }
+        }
+        public class Definition
+        {
+            public string definition {  get; set; }
+        }
+    }
+
+}
